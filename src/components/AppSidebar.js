c3b258a7a1a354f4501a77c00b5a6a15ec00a657
suriya94454 <suriyaecs22@gmail.com>
2024-07-24 16:31:05 +0530

    Logo changed

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-  CCloseButton,CSidebar,CSidebarBrand,CSidebarFooter,
-  CSidebarHeader,CSidebarToggler,} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { AppSidebarNav } from '../components/AppSidebarNav'
-import { logo } from '../assets/brand/logo'
-import { sygnet } from '../assets/brand/sygnet'
-import navigation from '../_nav'
+  CCloseButton,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarFooter,
+  CSidebarHeader,
+  CSidebarToggler,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { AppSidebarNav } from "../components/AppSidebarNav";
+import { sygnet } from "../assets/brand/sygnet";
+import ECSLOGO from "../assets/images/ecs_logo.png";
+import navigation from "../_nav";
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+  const dispatch = useDispatch();
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable);
+  const sidebarShow = useSelector((state) => state.sidebarShow);
 
   return (
     <CSidebar
@@ -22,28 +27,34 @@ const AppSidebar = () => {
       unfoldable={unfoldable}
       visible={sidebarShow}
       onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
+        dispatch({ type: "set", sidebarShow: visible });
       }}
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <div className="sidebar-brand-full">
+            <img className="img-fluid" alt="Logo" src={ECSLOGO} />
+          </div>
+          <div className="sidebar-brand-narrow">
+            <CIcon icon={sygnet} height={32} />
+          </div>
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
           dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+          onClick={() => dispatch({ type: "set", sidebarShow: false })}
         />
       </CSidebarHeader>
       <AppSidebarNav items={navigation} />
       <CSidebarFooter className="border-top d-none d-lg-flex">
         <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+          onClick={() =>
+            dispatch({ type: "set", sidebarUnfoldable: !unfoldable })
+          }
         />
       </CSidebarFooter>
     </CSidebar>
-  )
-}
+  );
+};
 
-export default React.memo(AppSidebar)
+export default React.memo(AppSidebar);
