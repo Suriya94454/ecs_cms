a90a7045554a355f64de33436d68fd4754c1abf5
Sridaran04dec <sri04122001@gmail.com>
2024-07-26 10:41:27 +0530

    updated

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import React, { useState } from "react";
+import {
+  AppContent,
+  AppSidebar,
+  AppFooter,
+  AppHeader,
+} from "../components/index";
+import SecondLayout from "./SecondLayout";
 
 const DefaultLayout = () => {
+  const [showSecondScreen, setShowSecondScreen] = useState(false);
+
+  const handleSecondScreenClick = () => {
+    setShowSecondScreen(true);
+  };
+
+  const handleMainScreenClick = () => {
+    setShowSecondScreen(false);
+  };
+
   return (
     <div>
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
-        <div className="body flex-grow-1">
-          <AppContent />
+      {!showSecondScreen ? (
+        <div>
+          <AppSidebar />
+          <div className="wrapper d-flex flex-column min-vh-100">
+            <AppHeader onSecondScreenClick={handleSecondScreenClick} />
+            <div className="body flex-grow-1">
+              <AppContent />
+            </div>
+            <AppFooter />
+          </div>
         </div>
-        <AppFooter />
-      </div>
+      ) : (
+        <div>
+          <SecondLayout onMainScreenClick={handleMainScreenClick} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
